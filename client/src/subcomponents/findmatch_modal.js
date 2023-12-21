554e73a56c6517790603e941844fa444cadef330
yun42 <unow1992@gmail.com>
2023-12-21 16:25:54 -0500

    Queue Status

--- client/src/subcomponents/findmatch_modal.js
+++ client/src/subcomponents/findmatch_modal.js
@@ -2,6 +2,8 @@ import React from 'react';
 import "../App.css";
 import {useState} from 'react';
 
+
+
 function Modal(props){
 	const matchTime = props.matchTime;
 	const matchDayIdx = props.matchDayIdx;
