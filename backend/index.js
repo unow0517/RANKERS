5545f87241d51633d34a6b775ae59937a9ce4e5a
root <unow1992@gmail.com>
2024-01-04 11:21:15 -0500

    buildmatch first process changed

--- backend/index.js
+++ backend/index.js
@@ -89,11 +89,13 @@ const matchtimes = ['10:00', '15:00']
 app.post('/buildmatch', async (req,res)=> {
 	const rd_day_idx = Math.floor(Math.random() * 7);
 	const rd_time_idx = Math.floor(Math.random() * 2);
-	const sql = "SELECT * FROM matchday" + rd_day_idx + " WHERE `time` =" + matchtimes[rd_time_idx] + " ORDER BY rating LIMIT 5";	
+	const sql = "SELECT * FROM matchday" + rd_day_idx + " WHERE `time` ='" + matchtimes[rd_time_idx] + "' ORDER BY rating LIMIT 5";	
 	var output = syncSql.mysql(config, sql)
-	// console.log("output",output.data.rows)
+	console.log("output.success",output.success)
+	console.log("output.data.rows.length",output.data.rows.length)
+
 	// console.log("1",output.data.rows[1])
-	if(output.success && output.length >= 2)
+	if(output.success && output.data.rows.length >= 2)
 	{
 		const shuffledArr = _.shuffle(output.data.rows)
 		user1 = shuffledArr[0]
@@ -103,8 +105,11 @@ app.post('/buildmatch', async (req,res)=> {
 		if(date)
 		{date = JSON.stringify(date).split('T')[0]+`"`}
 		time = user1.time;
-	} else {
-		return console.log("buildmatch first step failed at matchday" + rd_day_idx + ", " + matchtimes[rd_time_idx] )
+		console.log("buildmatch first process succeeded")
+	} else if (output.success && output.data.rows.length < 2) {
+		return console.log("fewer than 2 users in matchday" + rd_day_idx + ", " +matchtimes[rd_time_idx])
+	} else{
+		return console.log("buildmatch first step failed at matchday" + rd_day_idx + ", " + matchtimes[rd_time_idx], output)
 	}
 
 	const sql2 = "INSERT INTO matches (`uuid`,`user1_id`,`user1_email`,`user1_rating`,`user2_id`,`user2_email`,`user2_rating`,`date`,`time`) VALUES (uuid()," + user1.id + ",'" + user1.email + "'," + user1.rating + "," + user2.id + ",'" + user2.email + "'," + user2.rating + "," + date + ",'" + time +  "')"
